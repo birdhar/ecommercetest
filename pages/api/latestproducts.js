eba470a189a05addd629322afc3c34f3765795d1
birdhar <birat.dhar.89@gmail.com>
2024-03-18 11:59:20 +0530

    second commit

--- pages/api/latestproducts.js
+++ pages/api/latestproducts.js
@@ -1,8 +1,19 @@
-import { mongooseConnect } from "@/lib/mongoose";
+// import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 
 export default async function handle(req, res) {
-  await mongooseConnect();
-  const products = await Product.find().sort({ _id: -1 }).limit(10).exec();
+  // await mongooseConnect();
+  // const products = await Product.find().sort({ _id: -1 }).limit(10).exec();
+
+  const products = [
+    {
+      _id: "123424444444442",
+      image: "https://ecommercenext-birat.s3.amazonaws.com/1709671396105.jpg",
+      price: 500,
+      description: "sefhedvnk dgvui dfxh",
+      name: "kvdfv isdhvid v",
+      rating: 4,
+    },
+  ];
   res.json(products);
 }
