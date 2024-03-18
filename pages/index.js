e4dd8010eb4139b58ea158d69405365300471af4
birdhar <birat.dhar.89@gmail.com>
2024-03-18 11:39:03 +0530

    second commit

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,12 @@ import { getSession, useSession } from "next-auth/react";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 const HeroSection = dynamic(() => import("@/components/Herosection"));
-// const LatestProducts = dynamic(() => import("@/components/LatestProducts"));
+const LatestProducts = dynamic(() => import("@/components/LatestProducts"));
 // const Notification = dynamic(() => import("@/components/Notification"));
 // const Categories = dynamic(() => import("@/components/Categories"));
 // const AdSellFeature = dynamic(() => import("@/components/AdSellFeature"));
 
 function Home(params) {
-  const { data: session, status } = useSession();
-  console.log(session);
   return (
     <>
       <Head>
@@ -26,7 +24,8 @@ function Home(params) {
       </Head>
       <Layout>
         <HeroSection />
-        {/* <LatestProducts />
+        <LatestProducts />
+        {/* 
         <AdSellFeature />
         <Categories />
         <Notification /> */}
@@ -35,22 +34,22 @@ function Home(params) {
   );
 }
 
-// export async function getServerSideProps({ req }) {
-//   const session = await getSession({ req });
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
 
-//   if (!session) {
-//     return {
-//       redirect: {
-//         destination: `/login?next=${"/"}`,
-//         permanent: false,
-//       },
-//     };
-//   }
-//   return {
-//     props: {
-//       session,
-//     },
-//   };
-// }
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/login?next=${"/"}`,
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
+}
 
 export default Home;
