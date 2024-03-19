639fd22fb51a67ba695367c877a9ac4f5dd1fbea
birdhar <birat.dhar.89@gmail.com>
2024-03-19 10:37:49 +0530

    fourth commit

--- pages/products.jsx
+++ pages/products.jsx
@@ -363,22 +363,22 @@ function AllProducts() {
   );
 }
 
-// export async function getServerSideProps({ req }) {
-//   const session = await getSession({ req });
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
 
-//   if (!session) {
-//     return {
-//       redirect: {
-//         destination: `/login?next=${"/products"}`,
-//         permanent: false,
-//       },
-//     };
-//   }
-//   return {
-//     props: {
-//       session,
-//     },
-//   };
-// }
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/login?next=${"/products"}`,
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
+}
 
 export default AllProducts;
